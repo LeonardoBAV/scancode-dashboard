ba8cd89d139157c0cd3e91671fb76a2439b9fdcd
Leonardo Borges Athayde Vasconcelos <leobav1988@gmail.com>
2025-12-06 14:36:40 -0300

    Payment Domain

--- database/migrations/2025_11_20_203924_create_clients_table.php
+++ database/migrations/2025_11_20_203924_create_clients_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('fantasy_name')->nullable();
             $table->string('email')->nullable();
             $table->string('phone')->nullable();
+            $table->string('carrier')->nullable();
 
             $table->timestamps();
         });
